18eddb67d85ffa0b1c246f38a4abb5e5ab0464e9
paseri-kurosawa <mklmperson@gmail.com>
2026-03-08 18:51:54 +0900

    Initial commit

---
+++ test/bluesky-feed-jp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BlueskyFeedJp from '../lib/bluesky-feed-jp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bluesky-feed-jp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BlueskyFeedJp.BlueskyFeedJpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
